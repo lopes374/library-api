ed43b475e64b3b88db189e065e877252f8c58869
lopes374 <alex100d@icloud.com>
2020-07-26 11:21:32 -0300

    Corrrigindo os teste para subir o travis

--- src/main/java/com/sepol/libraryapi/api/resource/BookController.java
+++ src/main/java/com/sepol/libraryapi/api/resource/BookController.java
@@ -70,7 +70,7 @@ public class BookController {
 
     @PutMapping("{id}")
     @ApiOperation("Updates a book")
-    public BookDTO update( @PathVariable Long id, BookDTO dto){
+    public BookDTO update( @PathVariable Long id, @RequestBody BookDTO dto){
         log.info("updating book of id: {}", id);
         return service.getById(id).map( book -> {
 
--- src/test/java/com/sepol/libraryapi/api/resource/BookControllerTest.java
+++ src/test/java/com/sepol/libraryapi/api/resource/BookControllerTest.java
@@ -5,6 +5,7 @@ import com.sepol.libraryapi.exception.BusinessException;
 import com.sepol.libraryapi.model.entity.Book;
 import com.sepol.libraryapi.service.BookService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sepol.libraryapi.service.LoanService;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -46,6 +47,9 @@ public class BookControllerTest {
 
     @MockBean
     BookService service;
+
+    @MockBean
+    LoanService loanService;
 
     @Test
     @DisplayName("Deve criar um livro com sucesso.")
